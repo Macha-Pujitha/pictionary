d3af85b79078f8acd58bc658f00eedc083aea3ba
Macha Pujitha <machapujitha@gmail.com>
2017-05-09 00:32:33 +0530

    Delete TexttoSpeech.py

--- TexttoSpeech.py
+++
@@ -1,9 +0,0 @@
-'''
-To install it on anaconda
-pip install -i https://pypi.anaconda.org/pypi/simple pyttsx
-'''
-
-import pyttsx
-engine = pyttsx.init()
-engine.say('What the hell are you doin')
-engine.runAndWait()
